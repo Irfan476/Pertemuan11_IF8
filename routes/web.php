9297f03bf333845882833a9ea99b99471697b113
Irfan476 <irfanandryanto47@students.amikom.ac.id>
2023-07-09 19:48:14 +0700

    Commit pemweb laravel [template]

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\FrontController;
+use App\Http\Controllers\PostController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', [FrontController::class, 'index']);
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/posts', PostController::class);
+});
+
+require __DIR__.'/auth.php';
